992b2e5630229cf7a83e6507f3c33c893a9d0007
Kiril Gichevski <kirilgicevski@gmail.com>
2016-07-31 12:04:03 +0200

    Fix bug with trailing slash in URL

--- index.php
+++ index.php
@@ -5,10 +5,13 @@ include_once 'src/config/router.php';
 $requestUrl = $_SERVER['REQUEST_URI'];
 $requestUrlArray = explode('/', ltrim($requestUrl, '/'));
 
-$class = $requestUrlArray[0];
+$class = 'page';
+if (isset($requestUrlArray[0]) && $requestUrlArray[0] !== "") {
+    $class = $requestUrlArray[0];
+}
 
 $action = "index";
-if (isset($requestUrlArray[1])){
+if (isset($requestUrlArray[1]) && $requestUrlArray[1] !== ""){
     $action = $requestUrlArray[1];
 }
 $arguments = array_slice($requestUrlArray, 2);
